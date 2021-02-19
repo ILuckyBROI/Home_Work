eaaa5be0053dda87f8659c6a6860cdfcaee90f4d
ILuckyBROI <QalQ1@bk.ru>
2021-02-20 01:54:05 +0300

    Task_3

---
+++ Lesson_1/Task3.py
@@ -0,0 +1,32 @@
+#Реализовать склонение слова «процент» для чисел до 20.
+# Например, задаем число 5 — получаем «5 процентов»,
+# задаем число 2 — получаем «2 процента».Вывести все
+# склонения для проверки
+number = int(input('Введите число '))
+i = 0
+score = 0
+if number == 0:
+    print(number, '- процентов')
+elif number == 1:
+    print(number, '- процент')
+if number < 5:
+    print(number, '- процентa')
+else:
+    print(number, '- процентов')
+while i != 21:
+    if i == 0:
+        print(score, '- процентов')
+        i += 1
+        score += 1
+    elif i == 1:
+        print(score, '- процент')
+        i += 1
+        score += 1
+    elif i < 5:
+        print(score, '- процентa')
+        i += 1
+        score += 1
+    elif i <= 20:
+        print(score, '- процентов')
+        i += 1
+        score += 1
